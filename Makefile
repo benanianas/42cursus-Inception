c0b829df8f4facb6a1bb4b151dc2fa90735b39b5
benanianas <benanianas21@gmail.com>
2022-05-29 21:47:24 +0100

    makefile && cadvisor bonus

---
+++ Makefile
@@ -0,0 +1,15 @@
+init:
+	mkdir -p $HOME/data/wp $HOME/data/db
+up: 
+	docker-compose ./srcs/docker-compose.yml up --build 
+down:
+	docker-compose ./srcs/docker-compose.yml down
+clean:
+	rm -rf $HOME/data/wp $HOME/data/db
+fclean:
+	docker rm -f $(docker ps -a -q)
+	docker image rmi -f $(docker images -q)
+	docker network rm $(docker network ls -q)
+	docker volume rm -f $(docker volume ls -q)
+re:
+	clean fclean init up
--- srcs/requirements/bonus/website/tools/inception/pages/index.vue
+++ srcs/requirements/bonus/website/tools/inception/pages/index.vue
@@ -2,7 +2,7 @@
 <div class="cnt">
   <a href="https://abenani.42.fr" target="_blank"><div class="btn">Wordpress</div></a>
   <a href="https://adminer.abenani.42.fr" target="_blank"><div class="btn">Adminer</div></a>
-  <a href="http://localhost:9000" target="_blank"><div class="btn">CAdvisor</div></a>
+  <a href="http://localhost:3000" target="_blank"><div class="btn">CAdvisor</div></a>
   <a href="https://github.com/benanianas/42cursus-Inception" target="_blank"><div class="btn">Project's Repo</div></a>
 </div>
 </template>
