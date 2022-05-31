d0cb6325baaabc6c029472ac94f57e4f0966e9d6
Anas Benani <50434713+benanianas@users.noreply.github.com>
2022-05-31 20:33:53 +0100

    Update db.sh

--- srcs/requirements/mariadb/tools/db.sh
+++ srcs/requirements/mariadb/tools/db.sh
@@ -5,6 +5,6 @@ mysql -u root -e "CREATE DATABASE $DB_NAME" > /dev/null 2>&1
 mysql -u root -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED by '$MYSQL_PASSWORD'" > /dev/null 2>&1
 mysql -u root -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$MYSQL_USER'@'%'" > /dev/null 2>&1
 mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'" > /dev/null 2>&1
-killall -9 mysqld
+kill `cat /var/run/mysqld/mysqld.pid`
 mysqld_safe
 
